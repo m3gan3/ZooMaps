d8496e5ed75b0fa24ce9252e356a2013fb89099a
Mégane <mamichaud@smith.edu>
2018-03-06 18:26:24 -0500

    some models created

--- project2/GTech/ZooMaps/admin.py
+++ project2/GTech/ZooMaps/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, Place, Event
+admin.site.register(User)
+admin.site.register(Place)
+admin.site.register(Event)
--- project2/GTech/ZooMaps/models.py
+++ project2/GTech/ZooMaps/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    """
+    A typical class defining a model, derived from the Model class.
+    """
+
+    # Fields
+    username = models.CharField(max_length=20, help_text="Enter your username")
+   	
+   	
+    def __str__(self):
+        """
+        String for representing the MyModelName object (in Admin site etc.)
+        """
+        return self.username
+        
+class Place(models.Model):
+    """
+    A typical class defining a model, derived from the Model class.
+    """
+
+    # Fields
+    name = models.CharField(max_length=20, help_text="Enter the name of the Location")
+   	
+   	
+    def __str__(self):
+        """
+        String for representing the MyModelName object (in Admin site etc.)
+        """
+        return self.name
+        
+class Event(models.Model):
+    """
+    A typical class defining a model, derived from the Model class.
+    """
+
+    # Fields
+    name = models.CharField(max_length=20, help_text="Enter the name of the event")
+   	
+   	
+    def __str__(self):
+        """
+        String for representing the MyModelName object (in Admin site etc.)
+        """
+        return self.name
