85727ff445d21aaeb26075d82644926f8ae38210
m3gan3 <mamichaud@smith.edu>
2018-03-23 20:48:58 -0400

    template and index created

--- project2/GTech/ZooMaps/views.py
+++ project2/GTech/ZooMaps/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import User, Event, Tag, CommentEvent
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_user=User.objects.all().count()
+    num_event=Event.objects.all().count()
+    num_tag=Tag.objects.all().count()
+    num_comment=CommentEvent.objects.all().count()
+    
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_user':num_user,'num_event':num_event,'num_tag':num_tag,'num_comment':num_comment},
+    )
