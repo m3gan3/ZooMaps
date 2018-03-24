c120b4aa9bba2f67150ad326c2713f6b1f458c6a
m3gan3 <mamichaud@smith.edu>
2018-03-24 15:52:25 -0400

    form added

--- project2/GTech/GTech/settings.py
+++ project2/GTech/GTech/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ZooMaps.apps.ZoomapsConfig', 
+    
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    
+    'ZooMaps.apps.ZoomapsConfig',
 ]
 
 MIDDLEWARE = [
--- project2/GTech/ZooMaps/urls.py
+++ project2/GTech/ZooMaps/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.urls import include
 
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('account/', views.AccountListView.as_view(), name='account'),
 	path('events/', views.EventListView.as_view(), name='events'),
 	path('event/<int:pk>', views.EventDetailView.as_view(), name='event-detail'),
-
+	path('form/', include('django.contrib.flatpages.urls'),name='form'),
 ]
