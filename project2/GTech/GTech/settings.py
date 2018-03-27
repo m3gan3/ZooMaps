004985cbd8142f80658f33264201921bf09721a5
m3gan3 <mamichaud@smith.edu>
2018-03-27 16:44:58 -0400

    form fixed and admin fixed

--- project2/GTech/GTech/settings.py
+++ project2/GTech/GTech/settings.py
@@ -37,10 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    
     'ZooMaps.apps.ZoomapsConfig',
 ]
 
