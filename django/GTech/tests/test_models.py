4b3df1fcec975a4f99e2eaab34a9554b8c4ac0b9
curious-jor <chenjor@gmail.com>
2018-04-19 16:54:21 -0400

    TagTestCase functioning

--- django/GTech/tests/test_models.py
+++ django/GTech/tests/test_models.py
@@ -1,20 +1,21 @@
 from django.test import TestCase
-import ZooMaps.models
+from ZooMaps.models import Tag
 import random
 
 
 class TagTestCase(TestCase):
     def setUp(self):
         # Create list of tags with random names
+
         num_tags = random.randint(10, 35)
         tag_names = [str(random.randint(100, 1000)) for i in range(num_tags)]
         tags = [Tag.objects.create(tagName=name) for name in tag_names]
 
     def test_all_tags_return_their_names(self):
         # Tag '__str__' property should return the tagName property
-        tags = Tags.objects
+        tags = Tag.objects.all()
         for tag in tags:
-            assertEqual(tag.tagName, str(tag))
+            self.assertEqual(tag.tagName, str(tag))
 
     def test_get_absolute_url(self):
         # TODO, check that the url conf is defined
